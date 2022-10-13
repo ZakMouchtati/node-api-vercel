6d263485851d0357acd318264ee254da504cb159
Zak Mouchtati <zakaria.mouchtati01@gmail.com>
2022-10-13 17:12:33 +0100

    api-vercel

--- index.js
+++ index.js
@@ -10,34 +10,8 @@ app.listen(PORT, () => {
 })
 
 app.get('/', (req, res) => {
-    venom
-    .create(
-      'carrefour-chat-bot',
-      (base64Qr, asciiQR, attempts, urlCode) => {
-        console.log(asciiQR); // Optional to log the QR in the terminal
-        var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-          response = {};
-
-        if (matches.length !== 3) {
-          return new Error('Invalid input string');
-        }
-        response.type = matches[1];
-        response.data = new Buffer.from(matches[2], 'base64');
-
-        var imageBuffer = response;
-        console.log(imageBuffer['type'])
-        res.send(base64Qr)
-      },
-      undefined,
-      { logQR: false }
-    )
-    .then(async (client) => {
-      console.log('client has Benn Addd')
-      client.sendText('212621586010@c.us','Hello World Finally Done !!')
-    })
-    .catch((erro) => {
-      console.log(erro);
-    });
+    const channelKey = Math.random().toString(36).substr(2)
+    res.json({ channelKey })
 })
 
 app.get('/about', (req, res) => {
