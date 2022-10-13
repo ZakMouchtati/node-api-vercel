0d7179fd3c61c321e13f235b38a365316f72dcb8
Zak Mouchtati <zakaria.mouchtati01@gmail.com>
2022-10-13 17:53:44 +0100

    first

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const venom = require('venom-bot');
+const cors = require('cors')
+const path = require('path')
+
+const app = express();
+const httpServer = createServer(app);
+
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:8000",
+    // or with an array of origins
+    //   origin: ["http://127.0.0.1:5500", "http://localhost:3000"],
+    credentials: true
+  }
+})
+
+app.use(express.json());
+
+app.use(cors());
+
+let client = []
+
+app.get('/', (req, res) => {
+  const channelKey = Math.random().toString(36).substr(2)
+  res.json({ channelKey })
+        setInterval(() => {
+            console.log('Hello World')
+             io.sockets.emit(channelKey, {msg:"Hello WOrld !!"})
+        }, 300);
+
+})
+
+app.post('/send',async (req, res) => {
+
+    res.json({msg:'hello World'})
+})
+
+io.on("connection", (socket) => {
+  // socket.emit("hello", );
+
+});
+
+httpServer.listen(4000,() => {
+  console.log('App Has Stared On Port 4000')
+})
--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 const express = require('express')
-
-const venom = require('venom-bot');
-
+const venom = require('venom-bot')
+const { createServer } = require("http")
+const { Server } = require("socket.io")
 const app = express()
 const PORT = 4000
+const httpServer = createServer(app);
+const cors = require('cors')
+
+
+const io = new Server(httpServer, {
+    cors: {
+      origin: "http://localhost:8000",
+      // or with an array of origins
+      //   origin: ["http://127.0.0.1:5500", "http://localhost:3000"],
+      credentials: true
+    }
+  })
+  
+
+
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
+app.use(cors())
 
 app.get('/', (req, res) => {
     const channelKey = Math.random().toString(36).substr(2)
     res.json({ channelKey })
+    setInterval(() => {
+        io.sockets.emit(channelKey, {msg:'Hello World'});
+    }, 3000);
+
 })
 
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
 
+io.on("connection", (socket) => {
+    // socket.emit("hello", );
+  
+  });
+  
+
 // Export the Express API
 module.exports = app
+
+
